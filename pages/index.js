7d54a1707acf01bc810abf1b67f14ae0c236a748
Oliver Tuesta <oliver.jtuesta@gmail.com>
2022-12-16 16:42:05 -0500

    feat: add profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Box, Container, Heading } from '@chakra-ui/react';
+import {
+    Box,
+    Container,
+    Heading,
+    Image,
+    useColorModeValue,
+} from '@chakra-ui/react';
 
 const Home = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="glassTeal" p={3} mb={6} align="center">
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                p={3}
+                mb={6}
+                align="center"
+            >
                 Hi, I&apos;m a software engineer based in Lima, Peru.
             </Box>
             <Box display={{ md: 'flex' }}>
@@ -15,6 +27,22 @@ const Home = () => {
                         Developer, artist, and designer specializing in custom,
                         handmade work
                     </p>
+                </Box>
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    align="center"
+                >
+                    <Image
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        maxWidth="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        src="/images/spigi.jpg"
+                    />
                 </Box>
             </Box>
         </Container>
