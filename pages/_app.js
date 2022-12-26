963a343b3b5c94471b9dfc1f230f3d54d2738180
Oliver Tuesta <oliver.jtuesta@gmail.com>
2022-12-26 16:21:43 -0500

    feat: add vercel/analytics to enable Audiences on Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,21 @@ import Layout from '../components/layouts/main';
 import { AnimatePresence } from 'framer-motion';
 import theme from '../lib/theme';
 import Fonts from '../components/fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps, router }) {
     return (
-        <ChakraProvider theme={theme}>
-            <Fonts />
-            <Layout router={router}>
-                <AnimatePresence mode="wait" initial={true}>
-                    <Component {...pageProps} key={router.route} />
-                </AnimatePresence>
-            </Layout>
-        </ChakraProvider>
+        <>
+            <ChakraProvider theme={theme}>
+                <Fonts />
+                <Layout router={router}>
+                    <AnimatePresence mode="wait" initial={true}>
+                        <Component {...pageProps} key={router.route} />
+                    </AnimatePresence>
+                </Layout>
+            </ChakraProvider>
+            <Analytics />
+        </>
     );
 }
 
